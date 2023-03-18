689d1811c236164fc5d2af9df28bc562b03bebc7
SK SOHEL RAHAMAN <sohelrahaman43@gmail.com>
2023-03-18 16:42:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
-
+async function fetchData(callback) {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const data = await response.json();
+    callback(data);
+  }
+  
+  function displayMessage(data) {
+      document.getElementById("output").innerText = data.title;
+  }
+  // add onclick="fetchData(displayMessage)" on button in html.
 //your JS code here. If required.
